8bcd4e495c95e77c3f7793896f1965e0dcca6801
pj1124 <1124660146@qq.com>
2022-03-19 15:50:16 +0800

    恢复run.py文件误删代码

--- run.py
+++ run.py
@@ -2,6 +2,15 @@
 # @Author : Kevin
 # @Time : 2021/12/31 16:18
 
+from src.User.Api.user_api import app
+from src.ApiTest.ProjectConfig.Api.header_api import app
+from src.ApiTest.ProjectInfo.Api.project_api import app
+from src.ApiTest.ProjectConfig.Api.url_api import app
+from src.ApiTest.ProjectConfig.Api.mysql_api import app
+from src.Case.Api.case_name_api import app
+from src.Case.Api.case_data_api import app
+from src.File.Api.file_name_api import app
+from src.File.Api.file_data_api import app
 from src.TodoList.Api.todo_list_api import app
 
 app.run(host='192.168.1.102', port=8888, debug=True)
